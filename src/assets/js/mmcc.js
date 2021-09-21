1903f2ea43b45919c164809d8df36a4790d7a145
Ahmed Gewaly <ahmedgewaly@hotmail.com>
2021-09-22 00:34:11 +0200

    updates

--- src/assets/js/mmcc.js
+++ src/assets/js/mmcc.js
@@ -3,24 +3,24 @@ $('.dataTables-example').DataTable({
     pageLength: 25,
     responsive: true,
     dom: '<"html5buttons"B>lTfgitp',
-    buttons: [
-        { extend: 'copy' },
-        { extend: 'csv' },
-        { extend: 'excel', title: 'ExampleFile' },
-        { extend: 'pdf', title: 'ExampleFile' },
+    // buttons: [
+    //     { extend: 'copy' },
+    //     { extend: 'csv' },
+    //     { extend: 'excel', title: 'ExampleFile' },
+    //     { extend: 'pdf', title: 'ExampleFile' },
 
-        {
-            extend: 'print',
-            customize: function (win) {
-                $(win.document.body).addClass('white-bg');
-                $(win.document.body).css('font-size', '10px');
+    //     {
+    //         extend: 'print',
+    //         customize: function (win) {
+    //             $(win.document.body).addClass('white-bg');
+    //             $(win.document.body).css('font-size', '10px');
 
-                $(win.document.body).find('table')
-                    .addClass('compact')
-                    .css('font-size', 'inherit');
-            }
-        }
-    ]
+    //             $(win.document.body).find('table')
+    //                 .addClass('compact')
+    //                 .css('font-size', 'inherit');
+    //         }
+    //     }
+    // ]
  
 });
 
